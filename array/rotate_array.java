f65f947551984d84c5b31c4d18d5b3b8d0e54504
sakshamvishwakarma <sakshamvishwa98@gmail.com>
2026-08-26 20:56:52 +0530

    Add the rotate_array_code

--- array/rotate_array.java
+++ array/rotate_array.java
@@ -1,5 +1,34 @@
 package array;
 
 public class rotate_array {
-  
+
+    public static void main(String[] args) {
+
+        int[] arr = {45, 12, 89, 34, 67, 23, 91, 56};
+
+        int n = arr.length;
+        int d = 3;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+
+    static void reverse(int[] arr, int i, int j) {
+
+        while (i < j) {
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
 }
+
